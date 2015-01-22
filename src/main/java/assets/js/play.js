f440d7aa2b82522076a90cc73dd03ed6453522db
D3v01dZA <caltona1@gmail.com>
2015-01-22 15:20:48 +0200

    Add small js verification on play.html

--- src/main/java/assets/js/play.js
+++ src/main/java/assets/js/play.js
@@ -5,17 +5,24 @@ $(document).ready(function(){
 
     var count = $(".btn-select").length - 1;
     var siblings = [];
-    var i = 0;
     $("#userselection0").siblings(".dropdown-menu").children("li").siblings().each( function(){
-        siblings[i++] = $(this).children("a").html();
+        siblings[siblings.length] = $(this).children("a").html();
     });
-    for(i = 0; i < count; i++){
-        $("#userselection" + i).html(siblings[i] + "<span class=\"caret\"></span>");
+
+    for(var i = 0; i < count; i++){
+        $("#userselection" + i).html(siblings[i] + " <span class=\"caret\"></span>");
     }
 
     $(".dropdown-menu li a").click(function(e){
         var selText = $(this).text();
+        var old = $(this).parents('.btn-group').find('.dropdown-toggle').html();
         $(this).parents('.btn-group').find('.dropdown-toggle').html(selText+' <span class="caret"></span>');
+
+        var dup = checkDuplicates();
+        if(dup){
+            $(this).parents('.btn-group').find('.dropdown-toggle').html(old);
+            alert("No duplicate players allowed");
+        }
 
         if($(this).parent().parent().parent().attr("id") != "noplayers")
             e.preventDefault();
@@ -29,6 +36,28 @@ $(document).ready(function(){
         ajaxCeption(0);
     });
 });
+
+function checkDuplicates(){
+    var count = $(".btn-select").length - 1;
+    var siblings = [];
+    $("#userselection0").siblings(".dropdown-menu").children("li").siblings().each( function(){
+        siblings[siblings.length] = $.trim($(this).children("a").text());
+    });
+
+    var temp = 0;
+    for(var i = 0; i < siblings.length; i++){
+        for(var j = 0; j < count; j++){
+            if($.trim($("#userselection" + j).text()) == siblings[i]){
+                temp++;
+            }
+        }
+        if(temp > 1)
+            return true;
+        else
+            temp = 0;
+    }
+    return false;
+}
 
 function ajaxCeption(count){
     if(count < numberOfCeptions){
